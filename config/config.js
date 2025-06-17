91b163b3e469d7e65b93a0aa0b1b0a10d90c6f0a
SOCAIT <socait@Gianniss-MacBook-Pro.local>
2025-06-17 14:28:39 +0200

    secret issue resolved

--- config/config.js
+++ config/config.js
@@ -57,12 +57,6 @@ var vision_url = "https://ni9upzgef8.eu-west-3.awsapprunner.com/"
 //agent_url = "http://localhost:8080/chat"
 
 // backend_url ="http://34.102.246.190:80/api/"
-//token
-const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjUxNDEzNDU4LCJpYXQiOjE2NTE0MDk4NTgsImp0aSI6ImM2YWU4NTJmODU5MzQ0ZTU5NjE3ZWYyNzE0YmJmN2QzIiwidXNlcl9pZCI6N30.Oq1N45uSpn3MzYADepm-_5oCQGib5qwsn9XwWDfIhGc"
-
-const publishableKey = ""
-
-const lelouch_photo = "https://slifer.s3.amazonaws.com/Lelouch-Zero.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA5MWUER255TQW3LTS%2F20220718%2Feu-west-3%2Fs3%2Faws4_request&X-Amz-Date=20220718T112528Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=63c4c79d74efd3589b1bf44c6d0f1b6838ae3f1a8219743436a3fe1bd6703b28"
 
 const USDA_API_KEY = "F5Jya9rrfbPA5nBtVQmThPdQRajXLBPITdNZyoOS"
 
